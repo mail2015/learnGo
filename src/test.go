9a71d50756a9b98fb116133bcf39130ca6ab7e50
lidongxu test <mail2015@371223515.cn>
2015-06-03 15:31:08 +0800

    2015-06-03 lidongxu go demo

---
+++ src/test.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World 2015!")
+	fmt.Println("2 Hello World 2015!")
+	
+	fmt.Println("3 Hello World 2015!")
+	fmt.Println("4 Hello World 2015!")
+}
