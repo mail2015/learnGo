8d347ff7476a76788aa6c97575c16fb1be615cf0
lidongxu test <mail2015@371223515.cn>
2015-06-03 17:01:53 +0800

    2015-06-03 lidongxu demo2 基本语法 格式化

--- src/study/demo2/Demo2BaseGrammer.go
+++ src/study/demo2/Demo2BaseGrammer.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Demo2 base grammer")
+
+	fmt.Println("First line")
+
+	fmt.Println("Second line")
+
+	var age int
+	fmt.Println("age=", age)
+
+	var apples = "myApples "
+	var oranges = "greenOranges "
+	var fruit = apples + oranges
+	fmt.Println("fruit=", fruit)
+	fmt.Println("apples=", apples, "\t oranges=", oranges)
+
 }
