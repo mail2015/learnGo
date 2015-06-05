7e03804408912dad8645654ac6487ac6605f8e1c
LiDongxu <mail2015@371223515.cn>
2015-06-05 16:39:39 +0800

    2015-06-05 lidongxu demo7 case4 switch

--- src/study/demo7/Demo7Condition.go
+++ src/study/demo7/Demo7Condition.go
@@ -56,11 +56,65 @@ func methodIfElse(){
 
 func methodIfMultiple(){
 	fmt.Println(" \n\n \t if循环嵌套  ")
+	var a int = 100
+	var b int = 200
+	if(a == 100){
+		if(b == 200){
+			fmt.Println(" a value is 100 and b vlaue is 200 \n ")
+		}
+	}
+
+	fmt.Println(" case3 finish \n ")
 }
 
 
 func methodSwitch(){
 	fmt.Println(" \n\n \t switch语句 ")
+
+	var grade string  = "B"
+	var marks int = 90
+
+	switch marks{
+		case 90 : grade = "AA"
+		case 80 : grade = "BB"
+		case 50, 60, 70 : grade = "CC"
+		default : grade = "DD"  
+	}
+	fmt.Println(" marks is ", marks, "  grade is ", grade)
+
+	switch{
+		case grade == "AA" : 
+		  fmt.Println(" Excellent! ")
+		case grade == "BB" :
+		  fmt.Println(" Well done! ")
+		case grade == "CC" :
+		  fmt.Println(" Normal! ")
+		case grade == "DD" :
+		  fmt.Println(" Not pass! ")
+		default :
+		   fmt.Print(" N/A ")        
+	}
+
+	// switch type
+    var x interface{}
+
+    switch i := x.(type){
+    	case nil : 
+    	  fmt.Printf(" type of x : %T ", i)
+    	case int : 
+    	  fmt.Println(" type of x is int ")
+    	case float64 : 
+    	  fmt.Println(" type of x is float64 ")
+    	case func(int) float64 :
+    	  fmt.Println(" type of x is func(int) ") 
+    	case bool , string : 
+    	  fmt.Println(" type of x is bool or string ") 
+    	default : 
+    	  fmt.Printf(" type is x is %T ", i)  
+    }
+
+
+	fmt.Println(" case4 finish. marks is ", marks, "  grade is ", grade)
 }
 
 
