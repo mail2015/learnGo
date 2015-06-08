08ea8e0b822887370c847df3d2611ed08c19bf84
LiDongxu <mail2015@371223515.cn>
2015-06-08 16:11:28 +0800

    2015-06-08 lidongxu demo8 circulation for case

---
+++ src/study/demo8/Demo8Circulation.go
@@ -0,0 +1,65 @@
+package main
+	
+import "fmt"
+
+func main(){
+
+   forMethod();
+
+   forMultipleMethod();
+
+   breakStatement();
+
+   continueStatement();
+
+   gotoStatement();
+
+}
+
+
+
+func forMethod(){
+	fmt.Println(" \n\n \t for 循环 \n ")
+	var b int = 10
+	var a int
+
+	numbers := [6]int{1, 2, 3, 6}
+	/* for loop execution */
+	fmt.Println(" for case 1")
+	for i := 0; i < 10; i++{
+		fmt.Printf(" \n value of i is: %d ", i)
+	}
+	fmt.Println(" for case 2 -----")
+	for a < b {
+		a++
+		fmt.Println(" for a value is ", a, " for a value is ", b)
+	}
+	fmt.Println(" for case3 ")
+	for i, x :=range numbers{
+		fmt.Printf(" \n value od x = %d and i =  %d ", x, i)
+	}
+
+
+	fmt.Println(" \n\n a value is ", a, " b value is ", b, " numbers value is ", numbers)
+}
+
+func forMultipleMethod(){
+	fmt.Println(" \n\n \t 嵌套循环 ")
+
+}
+
+
+func breakStatement(){
+	fmt.Println(" \n\n \t break语句 ")
+}
+
+
+func continueStatement(){
+	fmt.Println(" \n\n \t  continue 语句 ")
+
+}
+
+func gotoStatement(){
+	fmt.Println(" \n\n \t goto语句 ")
+
+}
